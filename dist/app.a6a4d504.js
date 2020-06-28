c484f495261b974846dab4b0a9cb08f3fbadbc68
YjjTT <yjjtt@tempot.com>
2020-06-28 17:06:16 +0800

    add chai

--- dist/app.a6a4d504.js
+++ dist/app.a6a4d504.js
@@ -12740,7 +12740,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65123" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49178" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
