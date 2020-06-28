df0cb9106ba1ce90a5873c7cf69c4f026980d624
YjjTT <yjjtt@tempot.com>
2020-06-28 11:04:10 +0800

    parcel

---
+++ src/button.vue
@@ -0,0 +1,27 @@
+<template>
+    <button class="g-button">按钮</button>
+</template>
+<script>
+    export default {
+        name: "button"
+    }
+</script>
+<style lang="scss">
+    .g-button {
+        height: var(--button-height);
+        padding: 0 1em;
+        font: inherit;
+        border-radius: var(--border-radius);
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+        &:hover {
+            border-color: var(--border-color-hover);
+        }
+        &:active {
+            background-color: var(--button-active-bg);
+        }
+        &:focus {
+            outline: none;
+        }
+    }
+</style>
